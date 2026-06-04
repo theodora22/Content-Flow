f864039cc5548235d2d8c2fa60dfce3efe104cdb
Vicente Alvarez <veeveealvarez94@gmail.com>
2026-06-04 12:10:47 +0200

    Prevent user to enter anothers user ideas by just changing the URL

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,13 @@
 class IdeasController < ApplicationController
+  before_action :set_idea, only: [:show]
+
   def index
     @ideas = current_user.ideas.order(created_at: :desc)
+  end
+
+  private
+
+  def set_idea
+    @idea = current_user.ideas.find(params[:id])
   end
 end
